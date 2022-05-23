52113de12582a5f3777173682649f2ba3bd9facb
Daniel Feitosa dos Santos <daniel.f.a.santos@accenture.com>
2022-05-23 08:51:35 -0300

    encontro 4

--- buscar_indice.py
+++ buscar_indice.py
@@ -10,8 +10,14 @@ output -> indice: int
 """
 O (?)
 """
-def buscar_indice_binaria() -> int:
-    ...
+def buscar_indice_binaria(valor_buscado: int, lista: list) -> int:
+    inicio = 0
+    fim = len(lista) - 1
+
+    while inicio <= fim:
+        meio = (inicio + fim) // 2
+        if valor_buscado > lista[meio]:
+            inicio = meio + 1
 
 
 """
@@ -28,11 +34,11 @@ def buscar_indice_linear(valor_buscado: int, lista: list) -> int:
 
 
 lista =  list(range(1000))
-shuffle(lista)
+
+sorted(lista)
+print(lista)
 
 valor_buscado = int(input('Valor para buscar na lista: '))
 
 print(f' (l) O valor {valor_buscado} está presente na lista no índice {buscar_indice_linear(valor_buscado, lista)}.')
-# print(f' (b) O valor {valor_buscado} está presente na lista no índice {buscar_indice_binaria(valor_buscado, lista)}.')
-
-
+print(f' (b) O valor {valor_buscado} está presente na lista no índice {buscar_indice_binaria(valor_buscado, lista)}.')
